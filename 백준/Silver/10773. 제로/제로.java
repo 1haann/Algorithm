737646284211bfd3845a82f7c2765ffa0c522f44
SeWoong-Han <115627104+SeWoong-Han@users.noreply.github.com>
2023-12-15 01:21:17 +0900

    [Silver IV] Title: 제로, Time: 224 ms, Memory: 22772 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<K;i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0)
+                stack.pop();
+            else
+                stack.push(num);
+        }
+
+        int result = 0;
+        while(!stack.isEmpty()) {
+         result += stack.pop();
+        }
+        System.out.println(result);
+    }
+}
