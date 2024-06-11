0280a0b1f2381b6bac8dbff43d44e63b6d6bac5a
SeWoong-Han <115627104+SeWoong-Han@users.noreply.github.com>
2024-06-11 12:54:20 +0900

    [Bronze III] Title: 최댓값, Time: 136 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int maxValue = arr[0];
+        int maxIdx = 1;
+        for (int i = 1; i < 9; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                maxIdx = i + 1;
+            }
+        }
+        sb.append(maxValue + "\n");
+        sb.append(maxIdx + "\n");
+        System.out.println(sb);
+    }
+}
