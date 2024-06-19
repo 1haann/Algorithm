7ff9d2c65913bee4b50b7f37b70b79cd40c49ec1
SeWoong-Han <115627104+SeWoong-Han@users.noreply.github.com>
2024-06-19 14:32:23 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String[] str = new String[]{"         ,r'\"7","r`-_   ,'  ,/"," \\. \". L_r'","   `~\\/","      |","      |"};
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+    }
+}
